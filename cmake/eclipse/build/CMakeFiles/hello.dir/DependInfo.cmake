beac18506e36e66a6b7d65f798cb0956b9dc4ebc
Cedric Doucet <cedric.doucet@inria.fr>
2015-06-22 18:06:48 +0200

    Add en example of Eclipse project generation with CMake

---
+++ cmake/eclipse/build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cdoucet/Documents/forexample/cmake/eclipse/src/main.cpp" "/home/cdoucet/Documents/forexample/cmake/eclipse/build/CMakeFiles/hello.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ cmake/eclipse/src/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdlib>
+#include <iostream>
+int main()
+{
+  std::cout << "Hello world! It's me!" << std::endl;
+  return EXIT_SUCCESS;
+}
